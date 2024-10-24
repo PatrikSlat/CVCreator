ec059d75e304d99c6ed26f293c9eac26998ab55b
PatrikSlat <patrik.slat@gmail.com>
2024-10-24 15:58:40 +0200

    Added educational question sections

--- CVCreator/src/components/eduInfo.jsx
+++ CVCreator/src/components/eduInfo.jsx
@@ -1,7 +1,27 @@
+import { GrAdd } from "react-icons/gr";
+import { GrCheckmark } from "react-icons/gr";
+import { GrFormTrash } from "react-icons/gr";
+
+
 const EduInfoQ = () => {
-  return <>
-    <h1></h1>
-  </>
-}
+  return <div className="questions">
+  <h3>General Info:</h3>
+  <hr></hr>
+  <label for="name">School name:</label>
+  <input className="question" type="text" placeholder="University of California, Berkeley" name="school"></input>
+  <label for="tos">Title of Study:</label>
+  <input className="question" type="text" placeholder="Bachelor of Science in Computer Science" name="tos"></input>
+  <label for="date">Date of Study:</label>
+  <input className="question" type="text" placeholder="2003-2006" name="date"></input>  
+  <div id="space-left">
+    <GrFormTrash />
+    <GrAdd />
+    <GrCheckmark />
+  </div>
+  </div>
+  }
+
+
+
 
 export default EduInfoQ;
--- CVCreator/src/components/generalInfo.jsx
+++ CVCreator/src/components/generalInfo.jsx
@@ -2,7 +2,12 @@ const GeneralInfoQ = () => {
   return <div className="questions">
     <h3>General Info:</h3>
     <hr></hr>
-    <input type="text"></input>
+    <label for="name">Name:</label>
+    <input className="question" type="text" placeholder="E.g Joe Doe" name="name"></input>
+    <label for="email">E-mail:</label>
+    <input className="question" type="text" placeholder="E.g joe.doe@mail.com" name="email"></input>
+    <label for="phone">Phone:</label>
+    <input className="question" type="text" placeholder="(+49)1592313425" name="phone"></input>
   </div>
 }
 
